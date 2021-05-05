6a978cb606e8dd5ef911de943920d166a1eaf4f3
hb432 <hb432@protonmail.com>
2021-05-05 16:36:45 -0400

    fixed showstopping issues

--- src/hive.py
+++ src/hive.py
@@ -10,9 +10,10 @@ from typing import Dict
 import os
 
 def translate(tick, instructions, controller, physics):
+   edit = False
    duration = 0
    for instruction in instructions:
-      arguments = arguments.split(' ')
+      arguments = instruction.split(' ')
       if arguments[0] == 'wait':
          duration = duration + int(arguments[1])
       elif arguments[0] == 'boost':
@@ -70,20 +71,32 @@ def translate(tick, instructions, controller, physics):
                controller.pitch = min(max(float(arguments[2]), 0), 1)
          elif tick == duration + floor(max(float(arguments[3]), 0)):
             controller.pitch = 0
-      elif arguments[0] == 'position' or arguments[0] == 'velocity' or arguments[0] == 'angular-velocity':
+      elif arguments[0] == 'position':
          if tick == duration:
             physics.location.x = float(arguments[1])
             physics.location.y = float(arguments[2])
             physics.location.z = float(arguments[3])
-            return True
+            edit = True
       elif arguments[0] == 'rotation':
          if tick == duration:
             physics.rotation.roll = float(arguments[1]) * (pi / 180)
             physics.rotation.pitch = float(arguments[2]) * (pi / 180)
             physics.rotation.yaw = float(arguments[3]) * (pi / 180)
-            return True
-   return False
-
+            edit = True
+      elif arguments[0] == 'velocity':
+         if tick == duration:
+            physics.velocity.x = float(arguments[1])
+            physics.velocity.y = float(arguments[2])
+            physics.velocity.z = float(arguments[3])
+            edit = True
+      elif arguments[0] == 'angular-velocity':
+         if tick == duration:
+            physics.angular_velocity.x = float(arguments[1])
+            physics.angular_velocity.y = float(arguments[2])
+            physics.angular_velocity.z = float(arguments[3])
+            edit = True
+   return edit
+         
 session = { 'drones': [], 'mode': 0, 'script': {}, 'tick': 0 }
 
 script_folder = os.path.expanduser('~/Documents/Skrrripted')
@@ -99,40 +112,43 @@ class Hive(PythonHivemind):
    def get_outputs(self, packet: GameTickPacket) -> Dict[int, PlayerInput]:
       if packet.game_info.is_round_active:
          if session['mode'] == 0:
-            session['mode'] = 1
-            session['tick'] = 0
+            time = packet.game_info.game_time_remaining
+            if abs(time - floor(time)) < 0.001:
+               session['mode'] = 1
+               session['tick'] = 0
+               session['script'] = loads(open(script_file, 'r').read())
          else:
             edit = False
             state = GameState.create_from_gametickpacket(packet)
-            with session['script'] as script:
-               try:
-                  if (translate(session['tick'], script['ball'], None, state.ball.physics)):
-                     edit = True
-               except:
-                  pass
-               for drone in session['drones']:
-                  if drone['drone'] < len(script['drones']):
-                     try:
-                        if (translate(session['tick'], script['drones'][drone['drone']], drone['controller'], state.cars[drone['index']].physics)):
-                           edit = True
-                     except:
-                        pass
+            script = session['script']
+            try:
+               if (translate(session['tick'], script['ball'], None, state.ball.physics)):
+                  edit = True
+            except:
+               pass
+            for drone in session['drones']:
+               if drone['drone'] < len(script['drones']):
+                  try:
+                     if (translate(session['tick'], script['drones'][drone['drone']], drone['controller'], state.cars[drone['index']].physics)):
+                        edit = True
+                  except:
+                     pass
             if edit:
                self.set_game_state(state)
+            session['tick'] = session['tick'] + 1
       else:
          if session['mode'] == 1:
             session['mode'] = 0
-            session['script'] = loads(open(script_file, 'r').read())
             for drone in session['drones']:
-               with drone['vehicle'] as vehicle:
-                  vehicle.boost = False
-                  vehicle.handbrake = False
-                  vehicle.jump = False
-                  vehicle.pitch = 0
-                  vehicle.roll = 0
-                  vehicle.steer = 0
-                  vehicle.throttle = 0
-                  vehicle.yaw = 0
+               controller = drone['controller']
+               controller.boost = False
+               controller.handbrake = False
+               controller.jump = False
+               controller.pitch = 0
+               controller.roll = 0
+               controller.steer = 0
+               controller.throttle = 0
+               controller.yaw = 0
       controllers = {}
       for drone in session['drones']:
          if drone['index'] in self.drone_indices:
@@ -140,4 +156,4 @@ class Hive(PythonHivemind):
       return controllers
    def initialize_hive(self, packet: GameTickPacket) -> None:
       for index in self.drone_indices:
-         session['drones'].append({ 'index': index, 'control': PlayerInput(), 'drone': len(session['drones'])})
+         session['drones'].append({ 'index': index, 'controller': PlayerInput(), 'drone': len(session['drones'])})
